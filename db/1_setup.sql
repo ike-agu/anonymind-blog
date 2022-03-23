e964d9efe56d1477e6dfc8aa4580f9c7db73edf3
rakib2067 <rakib7@hotmail.co.uk>
2022-03-23 12:25:01 +0000

    fixed some errors with sql config

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -10,6 +10,6 @@ DROP TABLE IF EXISTS posts;
 CREATE TABLE posts(
   id serial PRIMARY KEY,
   title varchar(100) NOT NULL,
-  content varchar(500) NOT NULL
+  content varchar(500) NOT NULL,
   user_id int
 );
--- db/2_seed.sql
+++ db/2_seed.sql
@@ -6,6 +6,6 @@ VALUES
 
 INSERT INTO posts(title, content,user_id)
 VALUES
-      ('created my first docker-container', "it was awesome creating my first docker-container", 1)
-      ('Man-united lost today', "supporting this team is like a love hate relationship", 2),
-      ('overthrow the galaxy ', "My mission is to overthrow the galaxy and take over rthe entire universe", 3),
+      ('created my first docker-container', 'it was awesome creating my first docker-container', 1),
+      ('Man-united lost today', 'supporting this team is like a love hate relationship', 2),
+      ('overthrow the galaxy ', 'My mission is to overthrow the galaxy and take over rthe entire universe', 3);
