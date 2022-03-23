1babeae7ebbc7af01cd7cff7572a9e42bad75bac
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-03-23 11:44:00 +0000

    added  users index route in controller and require users route in server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-
+const userRoutes = require("./controllers/users")
+server.use('/users', userRoutes);
 
 
 server.get("/", (req, res) => res.send("Welcome to anonymind"));
