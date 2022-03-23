e0d21a7f0e1b590af15f5abbf960b5872c1fe1e1
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-03-23 20:00:57 +0000

    docker compose for test, folder for test and forlder for unit test and integration testing

---
+++ server/test/unit/models/Post.spec.js
@@ -0,0 +1,20 @@
+// const Post = require('../models/Post.spec')
+// const pg = require('pg')
+// jest.mock('pg');
+
+// const db = require('../../../dbConfig/init');
+
+// describe('user', () =>{
+//   beforeEach(() => jest.clearAllMocks())
+//   afterAll(() => jest.resetAllMocks())
+
+//   describe('all', () => {
+//     test('should return all users successfully from db', async() =>{
+//       jest.spyOn(db, 'query')
+//            .mockResolvedValueOnce({rows: [ {},{},{},{},{}]})
+//            const all = await User.all
+//            expect(all).toHaveLength(5)
+//     })
+//   });
+
+// })
